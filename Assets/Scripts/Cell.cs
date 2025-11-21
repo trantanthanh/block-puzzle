64d4d21a0203850d0ef6f3b997dc217ce8ca0cf6
Trần Tấn Thành <33313495+trantanthanh@users.noreply.github.com>
2025-11-22 01:11:00 +0700

    add board and board cells background, cell prefab

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    [SerializeField] private Sprite spriteNormal;
+    [SerializeField] private Sprite spriteHighlight;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.white;
+        spriteRenderer.sprite = spriteNormal;
+    }
+
+    public void Normal()
+    {
+        gameObject.SetActive(true);
+        spriteRenderer.sprite = spriteNormal;
+        spriteRenderer.color = Color.white;
+    }
+
+    public void Highlight()
+    {
+        gameObject.SetActive(true);
+        spriteRenderer.sprite = spriteHighlight;
+        spriteRenderer.color = Color.white;
+    }
+
+    public void Hover()
+    {
+        gameObject.SetActive(true);
+        spriteRenderer.sprite = spriteNormal;
+        spriteRenderer.color = new Color(spriteRenderer.color.r,
+            spriteRenderer.color.g,
+            spriteRenderer.color.b,
+            0.5f);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
